dcabbef020625222f71c9d3c0a933cf0aba6e1a0
echizenyayota <virtualmisso30@gmail.com>
2017-02-18 11:06:55 +0900

    P34 2.2.1.1 ユーザーロールを追加するアクションとしての最良のものは何か(修正)

--- wp-content/plugins/wpwa-user-manager/class-wpwa-user-manager.php
+++ wp-content/plugins/wpwa-user-manager/class-wpwa-user-manager.php
@@ -12,13 +12,13 @@ class WPWA_User_Manager {
     // 初期化コード
     register_activation_hook( __FILE__ , array( $this, 'add_application_user_roles' ) );
   }
+  // フォロワー、開発者、メンバー 3種類のユーザーロール
+  public function add_application_user_roles() {
+      add_role( 'follower', 'Follower', array( 'read' => true ) );
+      add_role( 'developer', 'Developer', array( 'read' => true ) );
+      add_role( 'member', 'Member', array( 'read' => true ) );
+  }
+
 }
 
 $user_manage = new WPWA_User_Manager;
-
-// フォロワー、開発者、メンバー 3種類のユーザーロール
-public function add_application_user_roles() {
-    add_role( 'follower', 'Follower', array( 'read' => true ) );
-    add_role( 'developer', 'Developer', array( 'read' => true ) );
-    add_role( 'member', 'Member', array( 'read' => true ) );
-}
